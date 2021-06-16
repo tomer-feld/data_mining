94b6b656236c5a44790b0618a1d628245eb3c31e
Tomer Feld <tomerfeld@gmail.com>
2021-06-16 19:19:09 +0300

    getting user handles

--- web_scraper.py
+++ web_scraper.py
@@ -11,7 +11,6 @@ url = "https://twitter.com/search?q=%23BlackLivesMatter&src=typeahead_click&lang
 try:
     driver.get(url)
     source = driver.page_source
-    print(type(source))
     soup = BeautifulSoup(source, 'lxml')
     sleep(5)
     elements = driver.find_elements_by_tag_name("article")
@@ -21,18 +20,22 @@ try:
 
         tweet_text = soup.find('div', attrs={
             'class': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0'})
-        print('Tweet: ', tweet_text.get_text())
-        if soup.find(text='Quote Tweet'):
-            retweet_text = soup.find('div', attrs={
-                'class': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-14gqq1x r-bcqeeo r-bnwqim r-qvutc0'})
+
+        user_handle = soup.findAll('div', attrs={
+            'class': 'css-901oao css-bfa6kz r-m0bqgq r-18u37iz r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0'})
+        print(f'Tweet by {user_handle[0].get_text()}: ', tweet_text.get_text())
 
         tweet_numbers = soup.findAll('div', attrs={'class': 'css-1dbjc4n r-xoduu5 r-1udh08x'})
         print(
             f'Comments: {tweet_numbers[0].get_text()}, Retweets: {tweet_numbers[1].get_text()}, Likes: {tweet_numbers[2].get_text()}')
-        print('Retweet: ', retweet_text.get_text())
+
+        if soup.find(text='Quote Tweet'):
+            retweet_text = soup.find('div', attrs={
+                'class': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-14gqq1x r-bcqeeo r-bnwqim r-qvutc0'})
+            print(f'Retweet of {user_handle[1].get_text()}: ', retweet_text.get_text())
         print()
         print()
     print(len(elements))
 finally:
-    # driver.quit()
+    driver.quit()
     pass

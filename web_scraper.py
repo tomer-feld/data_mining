15ae03121c8d910332f5fcecab6f7020f431eb77
Tomer Feld <tomerfeld@gmail.com>
2021-06-16 17:53:15 +0300

    getting tweet text with metrics

--- web_scraper.py
+++ web_scraper.py
@@ -17,9 +17,17 @@ try:
     elements = driver.find_elements_by_tag_name("article")
     for element in elements:
         soup = BeautifulSoup(element.get_attribute('innerHTML'), 'lxml')
-        res  = soup.find('div', attrs={'class':'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0'})
-        print(res.get_text())
-        # print(element.text)
+        # print(soup.prettify())
+        tweet_text  = soup.find('div', attrs={'class':'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0'})
+        print('Tweet: ',tweet_text.get_text())
+        # print(soup)
+        tweet_numbers = soup.findAll('div', attrs={'class':'css-1dbjc4n r-xoduu5 r-1udh08x'})
+        # print(tweet_numbers)
+        # for item in tweet_numbers:
+            # print(item.get_text())
+        print(f'Comments: {tweet_numbers[0].get_text()}, Retweets: {tweet_numbers[1].get_text()}, Likes: {tweet_numbers[2].get_text()}')
+        print()
+    print(len(elements))
 finally:
     # driver.quit()
     pass

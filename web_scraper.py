7be0c2d1bac87c5a52ae06b399e830a5744fde1a
Aaron Reuven <roni.reuven@gmail.com>
2021-06-16 16:57:29 +0300

    Got html objs of tweets

--- web_scraper.py
+++ web_scraper.py
@@ -3,23 +3,27 @@ from time import sleep
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
-PATH = 'C:\\Users\\tomer\\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
+# PATH = 'C:\\Users\\tomer\\chromedriver.exe'
+driver = webdriver.Firefox()
 url = "https://twitter.com/search?q=%23BlackLivesMatter&src=typeahead_click&lang=en"
 
 try:
     driver.get(url)
     source = driver.page_source
-    print(type(source))
+    # print(type(source))
     soup = BeautifulSoup(source, 'lxml')
     # print(soup.prettify())
-    sleep(10)
+    sleep(3)
     elements = driver.find_elements_by_tag_name("article")
     for element in elements:
         # print(type(element))
+        # print(element.id)
+        el = element.get_attribute('innerHTML')
+        souper = BeautifulSoup(el, 'lxml')
+        print(souper.prettify())
         # print(element.get_attribute('innerHTML'))
-        print(element.text)
-    # print(element)
+        # print(element.text)
+    print(len(elements))
 finally:
-    # driver.quit()
+    driver.quit()
     pass

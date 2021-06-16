5c7457a38fbfa9d6185e347489ffc563c7276f1d
Aaron Reuven <roni.reuven@gmail.com>
2021-06-16 17:20:20 +0300

    Trying to get multiple tweets

--- web_scraper.py
+++ web_scraper.py
@@ -15,15 +15,25 @@ try:
     # print(soup.prettify())
     sleep(3)
     elements = driver.find_elements_by_tag_name("article")
-    for element in elements:
+    soups = []
+    for el in elements:
+        soups.append(BeautifulSoup(el.get_attribute('innerHTML'),'lxml'))
+    while len(soups) < 20:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(1)
+        el = driver.find_element_by_tag_name('article')
+        # elements.append(el)
+        soups.append(BeautifulSoup(el.get_attribute('innerHTML'),'lxml'))
+    for element in soups:
         # print(type(element))
         # print(element.id)
-        el = element.get_attribute('innerHTML')
-        souper = BeautifulSoup(el, 'lxml')
-        print(souper.prettify())
+
+        print(element.prettify())
         # print(element.get_attribute('innerHTML'))
         # print(element.text)
-    print(len(elements))
+    # while len(elements) < 20:
+    #     elements.append(driver.find_element_by_tag_name('article'))
+    print(len(soups))
 finally:
     driver.quit()
     pass

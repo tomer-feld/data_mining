13b713279698aa5ae848e746349c5844b85ddd62
Tomer Feld <tomerfeld@gmail.com>
2021-06-22 16:06:07 +0300

    Added hashtags and mentions attributes and get methods. updated str method and added docstrings to all functions and methods.

--- web_scraper.py
+++ web_scraper.py
@@ -16,46 +16,75 @@ CHROME_TWEET_CLASSES = {
     'user_handle': 'css-901oao css-bfa6kz r-m0bqgq r-18u37iz r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0',
     'tweet_numbers': 'css-1dbjc4n r-xoduu5 r-1udh08x',
     'retweet_text': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-14gqq1x r-bcqeeo r-bnwqim r-qvutc0',
-    'timeline': 'css-1dbjc4n'
+    'timeline': 'css-1dbjc4n',
+    'tags': 'css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1n1174f r-1loqt21 r-poiln3 r-bcqeeo r-qvutc0'
 }
 # TODO: pick which languages we want to support
 SUPPORTED_LANGUAGES = ['en', 'it', 'und', 'es', 'fr']
 
 
 class Tweet:
+    """
+
+    """
     # TODO: Added mandatory properties still need to add optional arguments like quoted tweets. We might want to add
     #  links to comments or links to the tweet itself so we can use that data later or links to videos or images
-    def __init__(self, user_handle, time_of_tweet, tweet_text, stats=None, **properties):
+    def __init__(self, user_handle, time_of_tweet, tweet_text, tags, stats=None, **properties):
         self._user_handle = user_handle
         self._time = time_of_tweet
         self._text = tweet_text
         self._stats = stats  # TODO: What to do with this info
         if self._stats is None:
             self._stats = []
+        self._tags = tags
+        if self._tags is None:
+            self._tags = []
         self._properties = properties
 
     def get_time(self):
+        """returns the datetime stamp of the tweet in ISO format"""
         return self._time.time['datetime']
 
     def get_text(self):
+        """ returns tha plain text of the tweet"""
         return self._text.get_text()
 
     def get_user_handle(self):
+        """returns the user handle of the tweet poster"""
         return self._user_handle[0].get_text()
 
     def get_stats(self):
+        """returns the number of comments, retweets and likes"""
         return [stat.get_text() if stat.get_text() != '' else '0' for stat in self._stats]
 
     def get_properties(self):
         return self._properties
 
+    def get_hashtags(self):
+        """returns a list of all the hashtags used in the tweet"""
+        return [tag.get_text() for tag in self._tags if tag.get_text().startswith('#')]
+
+    def get_mentions(self):
+        """returns a list of al the users mentioned in the tweet"""
+        return [tag.get_text() for tag in self._tags if tag.get_text().startswith('@')]
+
     def __str__(self):
+        """displays the content of the tweet object"""
         return f'{self.get_time()}\nTweet by {self.get_user_handle()}: {self.get_text()}\n' \
                f'Comments: {self.get_stats()[0]}, Retweets: {self.get_stats()[1]},' \
-               f' Likes: {self.get_stats()[2]}'
+               f' Likes: {self.get_stats()[2]}\nMentions: {self.get_mentions()}\nHashtags: {self.get_hashtags()}'
 
 
 def scrape_hashtag(num, hashtag, top_or_live='live', driver=webdriver.Chrome(PATH)):
+    """
+    Opens the desired twitter hashtag page and returns a set containing all the raw tweets html data.
+    closes the page at the end
+    :param num: int. Number of desired tweets for scraping
+    :param hashtag: string. Hashtag for scraping
+    :param top_or_live: string. choose if scraping the top or the live tweets. default is 'live'
+    :param driver: the Selenium driver
+    :return: Set of raw tweets
+    """
     url = f"https://twitter.com/search?q=%23{hashtag}&lang=en&f={top_or_live}"
     try:
         driver.maximize_window()
@@ -65,7 +94,7 @@ def scrape_hashtag(num, hashtag, top_or_live='live', driver=webdriver.Chrome(PAT
         # saving the elements into the soups set because once we scroll down we lose the elements
         elements = []
         height = {'y': 0}
-        with tqdm(total=NUM) as pbar:
+        with tqdm(total=NUM) as pbar: # TODO: shouldn't that be num?
             while len(soups) < num:
                 # get last element and its location and scroll to its location
                 if len(elements) > 0:
@@ -81,12 +110,17 @@ def scrape_hashtag(num, hashtag, top_or_live='live', driver=webdriver.Chrome(PAT
                         soups.add(BeautifulSoup(element.get_attribute('innerHTML'), 'lxml'))
                         pbar.update(len(soups) - old)
     finally:
-        # driver.quit()
-        pass
+        driver.quit()
     return soups
 
 
 def extract_tweet_data(tweets, chrome_or_firefox='chrome'):
+    """
+    Creates a list of tweet objects of class Tweet from a set of raw html tweet elements.
+    :param tweets: iterable containing html tweet elements
+    :param chrome_or_firefox: specify from which browser the data was scraped. Relevant for class names
+    :return: a list of tweet objects
+    """
     # TODO: need to also allow firefox class names or better way to get tweet data
     extracted_tweets = []
     for tweet in tweets:
@@ -94,15 +128,10 @@ def extract_tweet_data(tweets, chrome_or_firefox='chrome'):
         tweet_text = tweet.find('div', attrs={'class': CHROME_TWEET_CLASSES['tweet_text']})
         user_handle = tweet.findAll('div', attrs={'class': CHROME_TWEET_CLASSES['user_handle']})
         tweet_numbers = tweet.findAll('div', attrs={'class': CHROME_TWEET_CLASSES['tweet_numbers']})
-
+        tags = tweet.findAll('a', attrs={'class': CHROME_TWEET_CLASSES['tags']})
         # TODO: still need to address the quoted tweet
-        # if len(user_handle) == 2:
 
-        # if tweet.find(text='Quote Tweet'):
-        #     retweet_text = tweet.find('div', attrs={
-                # 'class': CHROME_TWEET_CLASSES['retweet_text']})
-            # print(f'Retweet of {user_handle[1].get_text()}: ', retweet_text.get_text())
-        extracted_tweets.append(Tweet(user_handle, time_of_tweet, tweet_text, stats=tweet_numbers))
+        extracted_tweets.append(Tweet(user_handle, time_of_tweet, tweet_text, tags, stats=tweet_numbers))
     return extracted_tweets
 
 
@@ -117,6 +146,13 @@ def present_tweets(tweets):
 
 
 def save_to_csv(tweets, file_name, overwrite=False):
+    """
+    saves the data to a csv file
+    :param tweets: list of tweet objects
+    :param file_name: path to file
+    :param overwrite: choose whether to allow overwriting of existing file. Default=False
+    :return:
+    """
     if os.path.isfile(file_name) and not overwrite:
         print("File already exist. Didn't overwrite")
         return
@@ -136,7 +172,7 @@ def main():
         print(tweet.prettify())
     tweets = extract_tweet_data(raw_tweets)
     present_tweets(tweets)
-    save_to_csv(tweets, 'test.csv',overwrite=True)
+    # save_to_csv(tweets, 'test.csv',overwrite=True)
 
 
 if __name__ == '__main__':

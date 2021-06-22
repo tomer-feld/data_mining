ec1fe5494468a4191ed3f79b06a6800ccae0defd
Tomer Feld <tomerfeld@gmail.com>
2021-06-22 16:43:30 +0300

    Firefox dictionary added

--- web_scraper.py
+++ web_scraper.py
@@ -8,7 +8,8 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import pandas as pd
 
-PATH = 'C:\\Users\\tomer\\chromedriver.exe'
+# PATH = 'C:\\Users\\tomer\\chromedriver.exe'
+PATH = 'C:\\Users\\tomer\\geckodriver.exe'
 NUM = 1
 CHROME_TWEET_CLASSES = {
     'time_of_tweet': 'css-4rbku5 css-18t94o4 css-901oao r-m0bqgq r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0',
@@ -19,6 +20,15 @@ CHROME_TWEET_CLASSES = {
     'timeline': 'css-1dbjc4n',
     'tags': 'css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1n1174f r-1loqt21 r-poiln3 r-bcqeeo r-qvutc0'
 }
+FIREFOX_TWEET_CLASSES = {
+    'time_of_tweet': 'css-4rbku5 css-18t94o4 css-901oao r-m0bqgq r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0',
+    'tweet_text': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0',
+    'user_handle': 'css-901oao css-bfa6kz r-m0bqgq r-18u37iz r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0',
+    'tweet_numbers': 'css-1dbjc4n r-xoduu5 r-1udh08x',
+    'retweet_text': 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-14gqq1x r-bcqeeo r-bnwqim r-qvutc0',
+    'timeline': 'css-4rbku5 css-18t94o4 css-901oao r-m0bqgq r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0',
+    'tags': 'css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1n1174f r-1loqt21 r-poiln3 r-b88u0q r-bcqeeo r-qvutc0'
+}
 # TODO: pick which languages we want to support
 SUPPORTED_LANGUAGES = ['en', 'it', 'und', 'es', 'fr']
 
@@ -27,6 +37,7 @@ class Tweet:
     """
 
     """
+
     # TODO: Added mandatory properties still need to add optional arguments like quoted tweets. We might want to add
     #  links to comments or links to the tweet itself so we can use that data later or links to videos or images
     def __init__(self, user_handle, time_of_tweet, tweet_text, tags, stats=None, **properties):
@@ -75,7 +86,7 @@ class Tweet:
                f' Likes: {self.get_stats()[2]}\nMentions: {self.get_mentions()}\nHashtags: {self.get_hashtags()}'
 
 
-def scrape_hashtag(num, hashtag, top_or_live='live', driver=webdriver.Chrome(PATH)):
+def scrape_hashtag(num, hashtag, driver, top_or_live='live', ):
     """
     Opens the desired twitter hashtag page and returns a set containing all the raw tweets html data.
     closes the page at the end
@@ -94,7 +105,7 @@ def scrape_hashtag(num, hashtag, top_or_live='live', driver=webdriver.Chrome(PAT
         # saving the elements into the soups set because once we scroll down we lose the elements
         elements = []
         height = {'y': 0}
-        with tqdm(total=NUM) as pbar: # TODO: shouldn't that be num?
+        with tqdm(total=NUM) as pbar:  # TODO: shouldn't that be num?
             while len(soups) < num:
                 # get last element and its location and scroll to its location
                 if len(elements) > 0:
@@ -123,12 +134,16 @@ def extract_tweet_data(tweets, chrome_or_firefox='chrome'):
     """
     # TODO: need to also allow firefox class names or better way to get tweet data
     extracted_tweets = []
+    if chrome_or_firefox == 'chrome':
+        class_dict = CHROME_TWEET_CLASSES
+    else:
+        class_dict = FIREFOX_TWEET_CLASSES
     for tweet in tweets:
-        time_of_tweet = tweet.find('a', attrs={'class': CHROME_TWEET_CLASSES['time_of_tweet']})
-        tweet_text = tweet.find('div', attrs={'class': CHROME_TWEET_CLASSES['tweet_text']})
-        user_handle = tweet.findAll('div', attrs={'class': CHROME_TWEET_CLASSES['user_handle']})
-        tweet_numbers = tweet.findAll('div', attrs={'class': CHROME_TWEET_CLASSES['tweet_numbers']})
-        tags = tweet.findAll('a', attrs={'class': CHROME_TWEET_CLASSES['tags']})
+        time_of_tweet = tweet.find('a', attrs={'class': class_dict['time_of_tweet']})
+        tweet_text = tweet.find('div', attrs={'class': class_dict['tweet_text']})
+        user_handle = tweet.findAll('div', attrs={'class': class_dict['user_handle']})
+        tweet_numbers = tweet.findAll('div', attrs={'class': class_dict['tweet_numbers']})
+        tags = tweet.findAll('a', attrs={'class': class_dict['tags']})
         # TODO: still need to address the quoted tweet
 
         extracted_tweets.append(Tweet(user_handle, time_of_tweet, tweet_text, tags, stats=tweet_numbers))
@@ -156,9 +171,10 @@ def save_to_csv(tweets, file_name, overwrite=False):
     if os.path.isfile(file_name) and not overwrite:
         print("File already exist. Didn't overwrite")
         return
-    df = pd.DataFrame(columns=('user_handle', 'time_stamp', 'text', 'stats'))
+    df = pd.DataFrame(columns=('user_handle', 'time_stamp', 'text', 'stats', 'mentions', 'hashtags'))
     for tweet in tweets:
-        data = [tweet.get_user_handle(), tweet.get_time(), tweet.get_text(), tweet.get_stats()]
+        data = [tweet.get_user_handle(), tweet.get_time(), tweet.get_text(),
+                tweet.get_stats(), tweet.get_mentions(), tweet.get_hashtags()]
         row = pd.Series(data, index=df.columns)
         df = df.append(row, ignore_index=True)
     df.to_csv(file_name)
@@ -166,13 +182,13 @@ def save_to_csv(tweets, file_name, overwrite=False):
 
 # TODO: need to create function that exports tweets to file, and maybe also importer
 def main():
-    wd = webdriver.Chrome(PATH)
+    wd = webdriver.Firefox(PATH)
     raw_tweets = scrape_hashtag(NUM, 'BlackLivesMatter', driver=wd)
     for tweet in raw_tweets:
         print(tweet.prettify())
     tweets = extract_tweet_data(raw_tweets)
     present_tweets(tweets)
-    # save_to_csv(tweets, 'test.csv',overwrite=True)
+    save_to_csv(tweets, 'test.csv',overwrite=True)
 
 
 if __name__ == '__main__':
